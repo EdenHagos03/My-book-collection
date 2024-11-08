13a02eed06dfd14ffdec52cfc80fbb51034e769a
Eden Hagos <157740141+EdenHagos03@users.noreply.github.com>
2024-11-08 12:59:32 +0300

    Add files via upload

---
+++ back/controllers/booksController.js
@@ -0,0 +1,8 @@
+// const express = require('express')
+const conn = require('./../db')
+
+exports.getAllBooks = (req, res)=>{
+    //   conn.query('select * from Movies', (err, result)=>{
+    //     res.json(result)
+    res.end("akdfj");
+ }
